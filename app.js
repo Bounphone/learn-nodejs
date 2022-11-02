59363b0d78a64279d6e886a0c685f4cb5ad89932
Bounphone KOSADA <olaybounphonekosada@gmail.com>
2022-11-03 05:53:20 +0700

    complete

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 
 const app = express();
 
+app.use(express.json())
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
@@ -11,6 +13,21 @@ app.get('/api/v1/tours', (req, res) => {
         results : tours.length,
         data : tours
     })
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    const newID = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id : newID}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours) , err => {
+        res.status(201).json({
+            status : 'success',
+            // results : tours.length,
+            data : newTour
+        })
+    })
+    
 });
 
 const port = 3000;
