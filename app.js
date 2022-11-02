4f43edffccca4ea107a15be819459d3e4f5b5a62
Bounphone KOSADA <olaybounphonekosada@gmail.com>
2022-11-02 19:33:17 +0700

    basic express

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.status(200).json({msg : 'Hello from server'});
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: 'Hello from server' });
+});
+
+app.post('/', (req, res) => {
+    res.status(200).json({ msg: 'From post' });
 });
 
 const port = 3000;
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
